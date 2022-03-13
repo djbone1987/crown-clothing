c48cf9fd0e634e1a7b87b797c449f10213acd1a4
damienbone <damienbone@outlook.com>
2022-03-13 22:15:29 +0000

    Sign Up

--- src/pages/signin-register/signin-register.component.js
+++ src/pages/signin-register/signin-register.component.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './signin-register.style.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SigninRegisterPage = () => (
     <div className='signin-register'>
     <SignIn />
+        <SignUp />
     </div>
 )
 
